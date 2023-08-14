38b8aaa88f3dde7fc22ea9f1c93e016f5eed5a28
luspi1 <47425618+luspi1@users.noreply.github.com>
2023-08-14 14:54:32 +0300

    правки сканера qr

--- src/js/components/qrScanner.js
+++ src/js/components/qrScanner.js
@@ -5,12 +5,18 @@ const qrScanner = document.querySelector('#qrScanner')
 
 if (qrScanner) {
 
-  async function onScanSuccess(decodedText, decodedResult) {
-    // handle the scanned code as you like, for example:
-    // console.log(`Code matched = ${decodedText}`, decodedResult);
+  // новый код
+
+  const newCodeBtn = document.querySelector('.new-code-btn')
+
+  newCodeBtn.addEventListener('click', (e) => {
+    e.preventDefault()
+    location.reload()
+  })
+
+  async function onScanSuccess(decodedText) {
 
     const qrScanner = document.querySelector('#qrScanner')
-
     const dataUrl = qrScanner.dataset.script
 
     const data = {
@@ -28,6 +34,7 @@ if (qrScanner) {
         qrScanner.classList.add('hidden')
         qrReading.classList.remove('hidden')
         qrReading.classList.add(guest_status)
+        newCodeBtn.classList.remove('hidden')
       } else {
         showInfoModal(errortext)
       }
@@ -39,17 +46,12 @@ if (qrScanner) {
 
   }
 
-  function onScanFailure(error) {
-    showInfoModal(`Во время выполнения запроса произошла ошибка: ${error}`)
-  }
 
   let html5QrcodeScanner = new Html5QrcodeScanner(
     "qrScanner",
     {fps: 10, qrbox: {width: 250, height: 250}},
     /* verbose= */ false);
-  html5QrcodeScanner.render(onScanSuccess, onScanFailure);
-
-
+  html5QrcodeScanner.render(onScanSuccess);
 
 
 }
