fba95cc1459ef3290e742a485997f5eb7599f214
luspi1 <47425618+luspi1@users.noreply.github.com>
2024-08-14 11:39:22 +0300

    редизайн сканера на воротах

--- src/js/components/qrScanner.js
+++ src/js/components/qrScanner.js
@@ -5,16 +5,6 @@ const qrScanner = document.querySelector('#qrScanner')
 
 if (qrScanner) {
 
-  // новый код
-
-  const newCodeBtn = document.querySelector('.new-code-btn')
-
-  newCodeBtn.addEventListener('click', (e) => {
-    e.preventDefault()
-    location.reload()
-  })
-
-
   const submitQr = async (decodedText) => {
     const qrScanner = document.querySelector('#qrScanner')
     const dataUrl = qrScanner.dataset.script
@@ -28,19 +18,38 @@ if (qrScanner) {
       const response = await sendData(jsonData, dataUrl)
       const finishedResponse = await response.json()
 
-      const {status, errortext, guest_status, minutes} = finishedResponse
+      const {status, errortext, guest_status, fio, group_name, code, minutes} = finishedResponse
       if (status === 'ok') {
         const qrReading = document.querySelector('.qr-reading')
         const qrReadingMinutes = qrReading?.querySelector('span')
         qrScanner.classList.add('hidden')
         qrReading.classList.remove('hidden')
         qrReading.classList.add(guest_status)
-        newCodeBtn.classList.remove('hidden')
         if ((guest_status === '_cooldown') && qrReadingMinutes) {
           qrReadingMinutes.textContent = minutes
         }
+
+        if (guest_status === '_guest') {
+          const guestContainer = document.querySelector('.qr-scanner__one-guest')
+          const guestName = guestContainer.querySelector('.guest-name')
+          const guestCode = guestContainer.querySelector('.guest-code')
+
+          guestName.textContent = fio
+          guestCode.textContent = code
+          guestContainer.classList.remove('hidden')
+        }
+        if (guest_status === '_group') {
+          const groupContainer = document.querySelector('.qr-scanner__group')
+          const groupName = groupContainer.querySelector('.group-name')
+          const groupLeader = groupContainer.querySelector('.group-leader')
+          const groupCode = groupContainer.querySelector('.group-code')
+
+          groupName.textContent = group_name
+          groupLeader.textContent = fio
+          groupCode.textContent = code
+          groupContainer.classList.remove('hidden')
+        }
       } else {
-        newCodeBtn.classList.remove('hidden')
         showInfoModal(errortext)
       }
     } catch (err) {
